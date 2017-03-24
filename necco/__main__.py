a5b697ecda1ee2bee7d69dc4d451456171257031
Takashi Ando <dodo5522@gmail.com>
2017-03-24 11:46:14 +0900

    route.py -> views.pyにリネーム

--- necco/__main__.py
+++ necco/__main__.py
@@ -15,7 +15,7 @@
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
 
-from necco.route import app
+from necco.views import app
 
 
 def main(host="0.0.0.0", port=5000, debug=False):
