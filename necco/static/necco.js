e409ed4a65e5bb8d7f08693bb917d325f151dd5b
Takashi Ando <dodo5522@gmail.com>
2017-04-16 07:12:39 +0900

    jsの関数定義をglobalに

--- necco/static/necco.js
+++ necco/static/necco.js
@@ -1,16 +1,40 @@
+var activateNavbarItem = function(elementId) {
+  var activeItem = $("#navbar-item-list").children("li.active");
+  var targetItem = $(elementId);
+  activeItem.removeClass("active");
+  targetItem.addClass("active");
+};
+
+var showContent = function(idContent) {
+  $(".necco-content").css("display", "none");
+  $(idContent).css("display", "");
+};
+
+var prepareContent = function(type_) {
+  activateNavbarItem("#navbar-item-" + type_);
+  showContent("#id-necco-content-" + type_);
+
+  var content = $("#id-necco-content-" + type_);
+  content.empty();
+
+  var table = $("<table>").appendTo(content);
+  table.addClass("table");
+  table.addClass("table-responsive");
+  table.addClass("table-hover");
+
+  return table;
+};
+
+var sortRecordsByKana = function(records) {
+  return records.sort(
+    function(first, second) {
+      if(first.kana > second.kana) { return 1; }
+      else if(first.kana < second.kana) { return -1; }
+      else { return 0; }
+    })
+};
+
 $(function() {
-  var activateNavbarItem = function(elementId) {
-    var activeItem = $("#navbar-item-list").children("li.active");
-    var targetItem = $(elementId);
-    activeItem.removeClass("active");
-    targetItem.addClass("active");
-  };
-
-  var showContent = function(idContent) {
-    $(".necco-content").css("display", "none");
-    $(idContent).css("display", "");
-  };
-
   $("#navbar-item-passbook").on("click", function() {
     activateNavbarItem("#navbar-item-passbook");
     showContent("#id-necco-content-passbook");
@@ -18,32 +42,17 @@ $(function() {
 
   $("#navbar-item-settings").on("click", function() {
     activateNavbarItem("#navbar-item-settings");
+
+    $.ajax({
+      type: "GET",
+      url: "/api/account",
+      dataType: "json"
+    }).done(function(data, text, jqxhr){
+    }).fail(function(jqxhr, text, error){
+    });
+
     showContent("#id-necco-content-settings");
   });
-
-  var prepareContent = function(type_) {
-    activateNavbarItem("#navbar-item-" + type_);
-    showContent("#id-necco-content-" + type_);
-
-    var content = $("#id-necco-content-" + type_);
-    content.empty();
-
-    var table = $("<table>").appendTo(content);
-    table.addClass("table");
-    table.addClass("table-responsive");
-    table.addClass("table-hover");
-
-    return table;
-  };
-
-  var sortRecordsByKana = function(records) {
-    return records.sort(
-      function(first, second) {
-        if(first.kana > second.kana) { return 1; }
-        else if(first.kana < second.kana) { return -1; }
-        else { return 0; }
-      })
-    };
 
   $("#navbar-item-abilities").on("click", function() {
     var type_ = "abilities";
