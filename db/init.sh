0c8c04237893e7da9fa2e3767c3b0e6958791f95
Takashi Ando <dodo5522@gmail.com>
2017-05-11 13:28:42 +0900

    test用sqlite DB生成処理をpythonでリライト

--- db/init.sh
+++ db/init.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
 
-DB_FOR_TEST='/tmp/necco_test.db'
-SCHEME_FOR_TEST="$(dirname ${DB_FOR_TEST})/scheme.sql"
-
 function show_help() {
     echo $0 [-D] [-t] [-d DATABASE] [-u LOGIN_USER] [-p PASSWORD]
 }
 
 function parse_args() {
     local IS_DELETE_ALL=0
-    local IS_TEST=0
     local DB=necco
     local USER=temp
     local PASSWORD=temp
@@ -32,10 +28,6 @@ function parse_args() {
                 shift
                 PASSWORD=${1}
                 ;;
-            '-t' | '--test' )
-                shift
-                IS_TEST=1
-                ;;
             * )
                 ;;
         esac
@@ -44,38 +36,19 @@ function parse_args() {
         if [ -z "${1}" ]; then break; fi
     done
 
-    echo ${IS_TEST} ${IS_DELETE_ALL} ${DB} ${USER} ${PASSWORD}
+    echo ${IS_DELETE_ALL} ${DB} ${USER} ${PASSWORD}
 }
 
 function create_db() {
-    local IS_TEST=$1
-    local SCHEME=$2
-    local DATA_DIR=$3
-    local USER=$4
-    local PASSWORD=$5
+    local SCHEME=$1
+    local DATA_DIR=$2
+    local USER=$3
+    local PASSWORD=$4
     local DB_COMMAND=''
 
-    if [ ${IS_TEST} -ne 0 ]; then
-        DB_COMMAND="sqlite3 ${DB_FOR_TEST}"
-        cat ${SCHEME} | \
-            sed -e '/^CREATE DATABASE /d' | \
-            sed -e '/^USE /d' | \
-            sed -e 's/ INT / INTEGER /g' | \
-            sed -e 's/ BIGINT / INTEGER /g' | \
-            sed -e 's/ UNSIGNED / /g' | \
-            sed -e 's/ AUTO_INCREMENT */ AUTOINCREMENT /g' \
-            > ${SCHEME_FOR_TEST}
-
-        if [ -e "${DB_FOR_TEST}" ]; then
-            rm -f "${DB_FOR_TEST}"
-        fi
-
-        ${DB_COMMAND} < ${SCHEME_FOR_TEST}
-    else
-        DB_COMMAND="mysql -p${PASSWORD} -u ${USER}"
-        ${DB_COMMAND} < ${SCHEME}
-        DB_COMMAND="${DB_COMMAND} ${DB} -e"
-    fi
+    DB_COMMAND="mysql -p${PASSWORD} -u ${USER}"
+    ${DB_COMMAND} < ${SCHEME}
+    DB_COMMAND="${DB_COMMAND} ${DB} -e"
 
     for TABLE in $(ls ${DATA_DIR} | sed -e 's/\.txt//g')
     do
@@ -83,31 +56,20 @@ function create_db() {
         cat ${DATA_DIR}/${TABLE}.txt | while read VALUES
         do
             if [[ "${VALUES}" =~ ^# ]]; then
-                COLUMNS=$(echo ${VALUES} | sed -e 's/^#//')
                 continue
             fi
-            if [ ${IS_TEST} -eq 0 ]; then
-                ${DB_COMMAND} "INSERT INTO ${TABLE} VALUES (${VALUES})"
-            else
-                ${DB_COMMAND} "INSERT INTO ${TABLE}(${COLUMNS}) VALUES (${VALUES})"
-            fi
+            ${DB_COMMAND} "INSERT INTO ${TABLE} VALUES (${VALUES})"
         done
     done
 }
 
 function delete_db() {
-    local IS_TEST=$1
-    local USER=$2
-    local PASSWORD=$3
-    local DB=$4
+    local USER=$1
+    local PASSWORD=$2
+    local DB=$3
     local DB_COMMAND="mysql -p${PASSWORD} -u ${USER}"
 
-    if [ ${IS_TEST} -eq 0 ]; then
-        echo "DROP DATABASE ${DB};" | ${DB_COMMAND}
-    else
-        echo Deleting ${DB_FOR_TEST}...
-        rm -f ${DB_FOR_TEST}
-    fi
+    echo "DROP DATABASE ${DB};" | ${DB_COMMAND}
 }
 
 if [[ "${@}" =~ .*\ (-h|help)\ .* ]]; then
@@ -116,20 +78,14 @@ if [[ "${@}" =~ .*\ (-h|help)\ .* ]]; then
 fi
 
 RES=($(parse_args ${@}))
-IS_TEST=${RES[0]}
-IS_DELETE_ALL=${RES[1]}
-DB=${RES[2]}
-USER=${RES[3]}
-PASSWORD=${RES[4]}
-
-if [ ${IS_TEST} -ne 0 ]; then
-    DATA_DIR='test_data'
-else
-    DATA_DIR='data'
-fi
+IS_DELETE_ALL=${RES[0]}
+DB=${RES[1]}
+USER=${RES[2]}
+PASSWORD=${RES[3]}
+DATA_DIR='data'
 
 if [ ${IS_DELETE_ALL} -eq 0 ]; then
-    create_db ${IS_TEST} scheme.sql ${DATA_DIR} ${USER} ${PASSWORD}
+    create_db scheme.sql ${DATA_DIR} ${USER} ${PASSWORD}
 else
-    delete_db ${IS_TEST} ${USER} ${PASSWORD} ${DB}
+    delete_db ${USER} ${PASSWORD} ${DB}
 fi
