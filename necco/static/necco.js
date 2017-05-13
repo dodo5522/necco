ab551af1f2ee4ea964b50db28431a242f480ef9f
Takashi Ando <dodo5522@gmail.com>
2017-05-14 07:26:50 +0900

    PUT to /api/account時に成功/失敗alertを表示する

--- necco/static/necco.js
+++ necco/static/necco.js
@@ -75,7 +75,12 @@ $(function() {
       dataType: "json",
       data: sending_data
     }).done(function(data, text, jqxhr){
-      $("#contentSettings").trigger("click");
+      if(text==="success"){
+      	alert("成功しました");
+      }
+      else{
+      	alert("情報更新に失敗しました");
+      }
     }).fail(function(jqxhr, text, error){
       alert("失敗しました的なメッセージを表示しよう");
     });
